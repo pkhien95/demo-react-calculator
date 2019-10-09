c6a5e590699debc9b421cfabf54ec4d5e1197665
Hien Phung <pkhien95@gmail.com>
2019-10-09 23:51:09 +0700

    Remove unused import

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Home from "./home/Home";
 import Calculator from "./calculator/Calculator";
